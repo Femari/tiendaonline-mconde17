e48d6687c52abf87b117f1291915b1e80b403b8b
manuelcondegago@gmail.com <manuelcondegago@gmail.com@e28d53c5-e388-6653-6a42-50c4c0f2ad15>
2012-04-16 12:07:02 +0000

    Añadidas las clases del Paquete "Persistence" (están Vacías)

---
+++ TiendaOnline/src/java/Persistence/ProductDAOFile.java
@@ -0,0 +1,16 @@
+package Persistence;
+
+import Model.Product;
+
+public class ProductDAOFile implements ProductDAO {
+
+    @Override
+    public boolean newProduct(Product product) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean deleteProduct(Product product) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
