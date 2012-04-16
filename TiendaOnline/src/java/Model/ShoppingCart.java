08403e6c64d732422db12fd2227ed8af29d1d53a
manuelcondegago@gmail.com <manuelcondegago@gmail.com@e28d53c5-e388-6653-6a42-50c4c0f2ad15>
2012-04-16 11:36:16 +0000

    Modificadas las clases del paquete Model (Casi acabadas)

--- TiendaOnline/src/java/Model/ShoppingCart.java
+++ TiendaOnline/src/java/Model/ShoppingCart.java
@@ -1,5 +1,40 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class ShoppingCart {
-    
+
+    private ArrayList<Product> shoppingCart;
+
+    public ShoppingCart(ArrayList<Product> cart) {
+        shoppingCart = cart;
+    }
+
+    public ArrayList<Product> getShoppingCart() {
+        return this.shoppingCart;
+    }
+
+    public int getNumberOfProducts() {
+        return this.shoppingCart.size();
+    }
+
+    public void addProduct(Product product) {
+        this.shoppingCart.add(product);
+    }
+
+    public void removeProduct(Product product, int amount) {
+        this.shoppingCart.remove(product);
+    }
+
+    public float getPriceOfShoppingCart() {
+        float total = 0;
+        Product p;
+        Iterator it = shoppingCart.iterator();
+        while (it.hasNext()) {
+            p = (Product) it.next();
+            total = total + p.getPrice();
+        }
+        return total;
+    }
 }
