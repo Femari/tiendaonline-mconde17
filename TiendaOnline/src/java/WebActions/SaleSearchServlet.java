e7f8c5371f33aa387e76f4946090afb222871c8a
manuelcondegago@gmail.com <manuelcondegago@gmail.com@e28d53c5-e388-6653-6a42-50c4c0f2ad15>
2012-04-18 16:15:59 +0000

    Añadidos los métodos doGet y doPost (Vacíos) a todos los Servlets

--- TiendaOnline/src/java/WebActions/SaleSearchServlet.java
+++ TiendaOnline/src/java/WebActions/SaleSearchServlet.java
@@ -1,4 +1,20 @@
 package WebActions;
 
-public class SaleSearchServlet {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SaleSearchServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
 }
