4eb3ecb31666fd6d26c2f8d11b0684d291b17368
manuelcondegago@gmail.com <manuelcondegago@gmail.com@e28d53c5-e388-6653-6a42-50c4c0f2ad15>
2012-04-18 16:15:59 +0000

    Añadidos los métodos doGet y doPost (Vacíos) a todos los Servlets

--- trunk/TiendaOnline/src/java/WebActions/admin/EmailSendServlet.java
+++ trunk/TiendaOnline/src/java/WebActions/admin/EmailSendServlet.java
@@ -1,5 +1,20 @@
 package WebActions.admin;
 
-public class EmailSendServlet {
-    
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EmailSendServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
 }
