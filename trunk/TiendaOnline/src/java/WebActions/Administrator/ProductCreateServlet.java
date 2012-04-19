aae7dc26be95c00a7b2f9015acddf6be22957f0f
manuelcondegago@gmail.com <manuelcondegago@gmail.com@e28d53c5-e388-6653-6a42-50c4c0f2ad15>
2012-04-19 08:21:05 +0000

    Añadida Abstracción para los Servlets. Añadidos Scripts de creado y borrado de la Base de Datos

---
+++ trunk/TiendaOnline/src/java/WebActions/Administrator/ProductCreateServlet.java
@@ -0,0 +1,13 @@
+package WebActions.Administrator;
+
+import WebActions.ServletAbstraction;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProductCreateServlet extends ServletAbstraction {
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
---
+++ trunk/TiendaOnline/src/java/WebActions/ServletAbstraction.java
@@ -0,0 +1,28 @@
+package WebActions;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public abstract class ServletAbstraction extends HttpServlet {
+
+    protected abstract void processRequest(HttpServletRequest request, HttpServletResponse response);
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+}
