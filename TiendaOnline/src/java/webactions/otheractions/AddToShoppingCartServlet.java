f7fd1985a5bb223cbc9ea9ec345d151c825817b6
manuelcondegago@gmail.com <manuelcondegago@gmail.com@e28d53c5-e388-6653-6a42-50c4c0f2ad15>
2012-04-26 11:56:19 +0000

    Renombramiento de paquetes y páginas para adaptarlas a nomenclatura Java

---
+++ TiendaOnline/src/java/webactions/otheractions/AddToShoppingCartServlet.java
@@ -0,0 +1,37 @@
+package webactions.otheractions;
+
+import model.Product;
+import model.ShoppingCart;
+import webactions.AbstractServlet;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AddToShoppingCartServlet extends AbstractServlet {
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) {
+        response.setContentType("text/html;charset=UTF-8");
+        String productName = request.getParameter("productName");
+        String productAmount = request.getParameter("productAmount");
+        int amount = Integer.parseInt(productAmount);
+        ServletContext context = request.getServletContext();
+        Map<String, Product> productList = (Map<String, Product>) context.getAttribute("productList");
+        HttpSession session = request.getSession();
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+        Boolean authentication = (Boolean) session.getAttribute("authentication");
+        if (authentication == null) {
+            session.setAttribute("authentication", false);
+            Logger.getLogger(AddToShoppingCartServlet.class.getName()).log(Level.SEVERE,
+                    "El usuario no se ha registrado", authentication);
+        }
+        for (int i = 0; i < amount; i++) {
+            cart.addProduct(productList.get(productName));
+        }
+        goToURL(successPage, request, response);
+    }
+}
