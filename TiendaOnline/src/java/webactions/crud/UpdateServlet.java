fc9795b07dc69538fed19446fa5311b731ea20b8
manuelcondegago@gmail.com <manuelcondegago@gmail.com@e28d53c5-e388-6653-6a42-50c4c0f2ad15>
2012-05-07 09:32:34 +0000

    Añadidos todos los JavaScripts de validación

---
+++ TiendaOnline/src/java/webactions/crud/UpdateServlet.java
@@ -0,0 +1,29 @@
+package webactions.crud;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.Product;
+import persistence.PersistenceDAO;
+import persistence.ProductDAO;
+
+public class UpdateServlet extends CreateServlet {
+
+    @Override
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ProductDAO persistenceManagerProduct = PersistenceDAO.getProductDAO(persistenceMechanism);
+        String oldProductID = request.getParameter("oldProductID");
+        Product oldProduct = persistenceManagerProduct.getProduct(oldProductID);
+        Product newProduct = super.generateProductFromRequest(request);
+        newProduct.setProductID(oldProduct.getProductID());
+        if (persistenceManagerProduct.updateProduct(oldProduct, newProduct)) {
+            request.setAttribute("product", newProduct);
+            request.setAttribute("message", "Modificado con éxito");
+            goToURL(successForm, request, response);
+        } else {
+            goToURL(errorForm, request, response);
+        }
+    }
+}
