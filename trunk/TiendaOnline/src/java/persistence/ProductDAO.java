61d9310469b72dec0bcf0c6cfb804a419d2ff39b
manuelcondegago@gmail.com <manuelcondegago@gmail.com@e28d53c5-e388-6653-6a42-50c4c0f2ad15>
2012-05-11 14:05:07 +0000

    Solucionado Bug que permitía saltarse el login de administrador

---
+++ trunk/TiendaOnline/src/java/persistence/ProductDAO.java
@@ -0,0 +1,23 @@
+package persistence;
+
+import java.util.Map;
+import model.Product;
+
+public interface ProductDAO {
+
+    public boolean newProduct(Product product);
+
+    public boolean deleteProduct(Product product);
+
+    public boolean updateProduct(Product oldProduct, Product newProduct);
+
+    public Product getProduct(String productName);
+
+    public Map<String, Product> getProductMap();
+
+    public Map<String, Product> getProductMap(String parameter, String condition);
+
+    public boolean connection(String user, String pass, String destiny, String driver);
+
+    public boolean disconnect();
+}
